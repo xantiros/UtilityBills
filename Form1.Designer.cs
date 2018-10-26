2ecbc13dfd60dfc10cd6429e044cd7ea6d598b00
Xantiros <xantiros@gmail.com>
2018-10-26 14:52:57 +0200

    Release changes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -231,7 +231,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(919, 505);
+            this.ClientSize = new System.Drawing.Size(811, 394);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
